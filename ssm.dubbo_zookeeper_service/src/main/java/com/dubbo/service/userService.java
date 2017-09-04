f4287b2119b61431361b0c0eef60080009bde8fc
chenghongchao <206286062@qq.com>
2017-09-04 18:12:52 +0800

    创建ssm框架

--- ssm.dubbo_zookeeper_service/src/main/java/com/dubbo/service/userService.java
+++ ssm.dubbo_zookeeper_service/src/main/java/com/dubbo/service/userService.java
@@ -1,9 +1,9 @@
-package controller;
+package com.dubbo.service;
 
 /**
  * Created by chenghongchao on 2017/9/4.
  * Supported By 甜瓜移动.
  * Official Website: www.melonmobile.cn.
  */
-public class controller {
+public interface userService {
 }
