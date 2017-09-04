1c3bff26cc1492669d95836646b7ed377310cf8a
chenghongchao <206286062@qq.com>
2017-09-04 18:32:44 +0800

    创建ssm框架

---
+++ ssm.dubbo_zookeeper_service/src/main/java/com/dubbo/service/impl/userServiceImpl.java
@@ -0,0 +1,16 @@
+package com.dubbo.service.impl;
+
+import com.dubbo.service.userService;
+
+import java.util.Map;
+
+/**
+ * Created by chenghongchao on 2017/9/4.
+ * Supported By 甜瓜移动.
+ * Official Website: www.melonmobile.cn.
+ */
+public class userServiceImpl implements userService {
+    public int addUser(Map map) {
+        return 0;
+    }
+}
--- ssm.dubbo_zookeeper_service/src/main/java/com/dubbo/service/userService.java
+++ ssm.dubbo_zookeeper_service/src/main/java/com/dubbo/service/userService.java
@@ -1,9 +1,12 @@
 package com.dubbo.service;
 
+import java.util.Map;
+
 /**
  * Created by chenghongchao on 2017/9/4.
  * Supported By 甜瓜移动.
  * Official Website: www.melonmobile.cn.
  */
 public interface userService {
+    int addUser(Map map);
 }
